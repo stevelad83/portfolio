ffbd070f087dd7cff7c96771536279ba805a6e00
Steven Bryant <stevelad_83@yahoo.com>
2023-07-21 12:46:29 -0400

    extracted nav functionality into Navbar component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import About from "./components/about.js";
-import Contact from "./components/contact.js";
-import Projects from "./components/projects.js";
-import Home from "./components/home.js";
+import About from "./pages/about.js";
+import Contact from "./pages/contact.js";
+import Projects from "./pages/projects.js";
+import Home from "./pages/home.js";
+import Navbar from "./components/Navbar.js";
 
 function App() {
   return (
     <Router>
-      <div>
-        <nav className="navbar">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/projects">Projects</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
-
+      <Navbar />
+      <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
