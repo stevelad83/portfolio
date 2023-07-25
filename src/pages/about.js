fa3341b9d99dad4b45d71df3c19ef1fd35540d48
Steven Bryant <stevelad_83@yahoo.com>
2023-07-25 16:05:59 -0400

    started building about me page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,38 @@
 import React from "react";
+import profileImage from "../images/photo.jpeg";
+import "./about.css";
 
 export default function about() {
   return (
     <div>
-      <h2>About</h2>
-      <p>This is some info about me</p>
+      <h2>About Me</h2>
+      <img className="profile-image" src={profileImage} alt="My Profile" />
+      <p>
+        Hello, I'm Steve. I'm a junior software engineer with a background in
+        education and linguistics. I have a master's degree in Applied
+        Linguistics from{" "}
+        <a
+          href="https://www.liverpool.ac.uk/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          the University of Liverpool
+        </a>{" "}
+        and a bachelor's degree in Acting from{" "}
+        <a
+          href="https://www.leeds.ac.uk/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          the University of Leeds.{" "}
+        </a>
+      </p>
+      <p>
+        When I'm not writing code, you will find me hunting antiques, cooking,
+        meditating, or exploring new places with my wife, daughters, cats and
+        guitar.{" "}
+      </p>
     </div>
   );
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,30 +10,57 @@ const handleSubmit = (e) => {
   const email = formData.get("email");
   const message = formData.get("message");
   console.log(name, email, message);
-  // window.location.href = `mailto:stevelad_83@yahoo.com?subject=Contact%20Form&body=Name:%20${name}%0AEmail:%20${email}%0AMessage:%20${message}`;
+  window.location.href = `mailto:stevelad_83@yahoo.com?subject=Contact%20Form&body=Name:%20${name}%0AEmail:%20${email}%0AMessage:%20${message}`;
 };
 
 export default function contact() {
   return (
     <div className="container">
-      <h2>Contact</h2>
-      <p>Contact me here...</p>
+      <h1 className="contact-h1">Get in touch</h1>
+      <p>
+        I'm always happy to connect over ☕ (virtual or actual) or 🍺!<br></br>
+        Please send your message using the form below.
+      </p>
       <form className="contact-form" onSubmit={handleSubmit}>
-        <label htmlFor="name" className="contact-label">
-          Name
-        </label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          className="contact-input"
-          required
-        />
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" required />
-        <label htmlFor="message">Message</label>
-        <textarea id="message" name="message" rows="5" required></textarea>
-        <button type="submit">Send</button>
+        <div className="contact-div">
+          <label htmlFor="name" className="contact-label">
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="contact-input"
+            required
+          />
+        </div>
+        <div className="contact-div">
+          <label htmlFor="email" className="contact-label">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="contact-input"
+            required
+          />
+        </div>
+        <div className="contact-div">
+          <label htmlFor="message" className="contact-label">
+            Message
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            rows="5"
+            className="contact-input"
+            required
+          ></textarea>
+        </div>
+        <button type="submit" className="contact-button">
+          Send
+        </button>
       </form>
     </div>
   );
