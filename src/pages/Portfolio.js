ea965452dab9a257b74db4dbd95f82719a10cc9f
Steven Bryant <stevelad_83@yahoo.com>
2023-07-27 10:34:41 -0400

    styling project cards

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ProjectCard from "../Components/ProjectCard.js";
 import projects from "../data/projectData.js";
+import "./portfolio.css";
 
 export default function Portfolio() {
   return (
     <div>
       <h2>Projects</h2>
       <p>Here are some projects I have worked on</p>
-      <div className="project-grid">
+      <div className="project-container">
         {projects.map((project) => (
           <ProjectCard key={project.id} project={project} />
         ))}
