67d505f4bdfd9218afe1499557d78227763392df
Steven Bryant <stevelad_83@yahoo.com>
2023-07-28 10:54:58 -0400

    added placeholder images to ProjectCard

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,13 +1,34 @@
+// import React from "react";
+// import "./project-card.css";
+
+// export default function ProjectCard({ project }) {
+//   return (
+//     <div className="project-card">
+//       <img src={project.image} alt={project.title} />
+//       <h3>{project.title}</h3>
+//       <p>{project.description}</p>
+//       <a href={project.link}>Link to project</a>
+//     </div>
+//   );
+// }
+
 import React from "react";
 import "./project-card.css";
+import "../data/projectData.js";
 
-export default function ProjectCard({ project }) {
+const ProjectCard = ({ project }) => {
+  const { title, description, link } = project;
+
   return (
     <div className="project-card">
-      <img src={`../images/${project.image}`} alt={project.title} />
-      <h3>{project.title}</h3>
-      <p>{project.description}</p>
-      <a href={project.link}>Link to project</a>
+      <img src={require(`../images/photo.jpeg`)} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <a href={link} target="_blank" rel="noopener noreferrer">
+        View Project
+      </a>
     </div>
   );
-}
+};
+
+export default ProjectCard;
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,13 +2,17 @@ import React from "react";
 import ProjectCard from "../Components/ProjectCard.js";
 import projects from "../data/projectData.js";
 import "./portfolio.css";
+import "../images/photo.jpeg";
 
 export default function Portfolio() {
   return (
     <div>
       <h2>Projects</h2>
       <p>Here are some projects I have worked on</p>
-      <div className="project-container">
+      <div
+        className="project-container"
+        // style={{ backgroundImage: `url(${require(`../images/photo.jpeg`)})` }}
+      >
         {projects.map((project) => (
           <ProjectCard key={project.id} project={project} />
         ))}
