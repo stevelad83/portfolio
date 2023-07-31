3266531ff88d69c178747843c67a4455b46183e1
Steven Bryant <stevelad_83@yahoo.com>
2023-07-31 14:52:02 -0400

    changed position on footer to allow z-indexing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,15 @@ export default function Footer() {
       </p>
       <p>
         Illustrations courtesy of{" "}
-        <a href="https://icons8.com/illustrations">Ouch!</a>
+        <a href="https://icons8.com/illustrations">Ouch!</a> <br></br>
+        GIF by{" "}
+        <a href="https://pixabay.com/users/prawny-162579/?utm_source=link-attribution&utm_medium=referral&utm_campaign=animation&utm_content=6132">
+          Prawny
+        </a>{" "}
+        from{" "}
+        <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=animation&utm_content=6132">
+          Pixabay
+        </a>
       </p>
     </footer>
   );
